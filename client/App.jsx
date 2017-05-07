99b356f7229ca62c56b62a5140e15b14c902dd5e
zc0311 <zc0311@gmail.com>
2017-05-06 22:36:20 -0700

    Search

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Search from './components/Search'
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>This is the App container, all pages will be rendered inside this bro.</h1>
+        <h2>This is where react-router should do some shit.</h2>
+        <Search />
       </div>
     )
   }
