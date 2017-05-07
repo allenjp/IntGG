5e4a03906330e58da8f59c17cffbf7b2ac2e73f6
Jeff <jeffreypallen3@gmail.com>
2017-05-07 02:21:45 -0400

    More updates to api scrape

--- api/api_main.js
+++ api/api_main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-    var summoner_obj = getSummonerObj('jpallen');
+    //var summoner_obj = getSummonerObj('jpallen');
+    kickoff('jpallen');
 });
 
+function kickoff(summonerName) {
+    getSummonerObj(summonerName);
+}
 function getSummonerObj(summonerName) {
     var api_url = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/" + summonerName + "?api_key=3a0fbaee-bea5-48fe-bcc6-0581cf9407e7";
     
@@ -11,18 +15,71 @@ function getSummonerObj(summonerName) {
             "summoner_name": data.name,
             "account_id": data.accountId
         }
-        
-        getMatchList(data.accountId)
-        
+        getMatchIDList(data.accountId)
     });
 }
 
-function getMatchList(accountID) {
+function getMatchIDList(accountID) {
     var api_url = "https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/" + accountID + "/recent?api_key=3a0fbaee-bea5-48fe-bcc6-0581cf9407e7"
     
-    var matchIDList = [];
+    $.get(api_url, function(data) {
+        $.each(data.matches, function(k, v) {
+            getMatch(v.gameId, accountID);
+        });
+    });
+}
+
+function getMatch(gameID, accountID) {
+    var participantID = getParticipantID(gameID, accountID);
+}
+
+function getParticipantID(gameID, accountID) {
+    var api_url = "https://na1.api.riotgames.com/lol/match/v3/matches/" + gameID + "?api_key=3a0fbaee-bea5-48fe-bcc6-0581cf9407e7";
+    
+    var participantID = 0;
+    
+    $.get(api_url, function (data) {
+        $.each(data.participantIdentities, function(k, player) {
+           if (player.player.currentAccountId == accountID) {
+               participantID = player.participantId;
+           }
+        });
+        getMatchDetails(gameID, participantID);
+    });
+}
+
+function getMatchDetails(gameID, participantID) {
+    
+    var api_url = "https://na1.api.riotgames.com/lol/match/v3/timelines/by-match/" + gameID + "?api_key=3a0fbaee-bea5-48fe-bcc6-0581cf9407e7";
+    
+    var death_objs = [];
     
     $.get(api_url, function(data) {
         console.log(data);
+        $.each(data.frames, function (k, frame) {
+            $.each(frame.events, function (j, event) {
+                if (event.type.toLowerCase() == "champion_kill" && event.victimId == participantID) {
+                    var death_obj = {
+                        "type": event.type,
+                        "timestamp": event.timestamp,
+                        "position": {
+                            "x": event.position.x,
+                            "y": event.position.y
+                        },
+                        "killerId": event.killerId,
+                        "victimId": event.victimId
+                    }
+                    death_objs.push(death_obj);
+                }
+            })
+        });
+        
+        // CHANGE THIS:
+        // Make victim id == participant id of active user
+        
+        // also grab champion portrait based on champ id
+        
+        console.log(death_objs);
     });
+    
 }
