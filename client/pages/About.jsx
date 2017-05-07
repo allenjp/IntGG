b6388ea7a23da7454ad01aab17b82b7d77f86bb8
zc0311 <zc0311@gmail.com>
2017-05-07 15:11:57 -0700

    React-router pretty much working

--- client/pages/About.jsx
+++ client/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class About extends React.Component {
   render() {
     return (
-      <h1>About</h1>
+      <div>
+        <h1>About</h1>
+        <Link to="/">Home</Link>
+      </div>
     )
   }
 }
--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Search from '../components/Search'
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
+        <Link to="/about">About</Link>
         <h2>featured feeders banner</h2>
+        <Search />
       </div>
     )
   }
