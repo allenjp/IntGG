e59c266247fffea88dc62cd91326215b595a9648
Jeff <jeffreypallen3@gmail.com>
2017-05-09 23:05:31 -0400

    Adding api dependency to server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./client/public'));
+app.use(express.static('.server/services/api'));
 
 app.listen(8080, function() {
   console.log("WERE UP AND RUNNING (IT DOWN MID)");
-})
+});
