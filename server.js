06af4ae34fe51738f8ddcf5f6a37cfd8a8cfb400
Jeff <jeffreypallen3@gmail.com>
2017-05-09 22:09:24 -0400

    folder structure mods

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-var routes = require('./api/routes/apiRoutes.js');
-routes(app);
-
-app.use(express.static('./public'));
-app.use(express.static('./api'));
+app.use(express.static('./src/views'));
 
 app.listen(8080, function() {
-  console.log("WE'RE UP AND RUNNING (IT DOWN MID)");
+  console.log("WERE UP AND RUNNING (IT DOWN MID)");
 })
