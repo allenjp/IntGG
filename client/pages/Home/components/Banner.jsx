2cbee291b10edf3f80faca2a7c81eb2a92d39402
zc0311 <zc0311@gmail.com>
2017-05-09 19:39:38 -0700

    File restructuring

---
+++ client/pages/Home/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>featured feeders banner</h2>
+      </div>
+    )
+  }
+}
+
+export default Banner;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./public'));
+app.use(express.static('./client/public'));
 
 app.listen(8080, function() {
   console.log("WE'RE UP AND RUNNING (IT DOWN MID)");
