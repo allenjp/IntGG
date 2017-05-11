c22ce722444b9e49cf5cfccbd61a228cc990e67d
Jeff <jeffreypallen3@gmail.com>
2017-05-10 22:13:05 -0400

    Finalizing scrape - still have to figure out rate limiting

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('./client/public'));
-app.use(express.static('.server/services/api'));
 
-app.listen(8080, function() {
+var routes = require('./server/services/api/routes/apiRoutes');
+routes(app);
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
   console.log("WERE UP AND RUNNING (IT DOWN MID)");
 });
