776de20f168dafcbc909e5949e68a3da09fc7ba4
zc0311 <zc0311@gmail.com>
2017-05-11 18:39:15 -0700

    .gitignore buggy

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -18376,7 +18376,7 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRouterDom = __webpack_require__(127);
 
-var _PlayerStats = __webpack_require__(480);
+var _PlayerStats = __webpack_require__(217);
 
 var _PlayerStats2 = _interopRequireDefault(_PlayerStats);
 
@@ -18539,7 +18539,109 @@ var MadLib = function (_React$Component) {
 exports.default = MadLib;
 
 /***/ }),
-/* 217 */,
+/* 217 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactBootstrap = __webpack_require__(361);
+
+var _MadLib = __webpack_require__(216);
+
+var _MadLib2 = _interopRequireDefault(_MadLib);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// var death_obj = {
+//     "type": event.type,
+//     "timestamp": event.timestamp,
+//     "position": {
+//         "x": event.position.x,
+//         "y": event.position.y
+//     },
+//     "killerId": event.killerId,
+//     "victimId": event.victimId
+// }
+
+var PlayerStats = function (_React$Component) {
+  _inherits(PlayerStats, _React$Component);
+
+  function PlayerStats(props) {
+    _classCallCheck(this, PlayerStats);
+
+    var _this = _possibleConstructorReturn(this, (PlayerStats.__proto__ || Object.getPrototypeOf(PlayerStats)).call(this, props));
+
+    _this.state = {
+      test: 'testing testing'
+    };
+    return _this;
+  }
+
+  _createClass(PlayerStats, [{
+    key: 'handleSubmit',
+    value: function handleSubmit(input) {
+      fetch('/getdeathinfo/' + input).then(function (data) {
+        console.log(data);
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      console.log(this.state.test);
+      return _react2.default.createElement(
+        'div',
+        null,
+        _react2.default.createElement(
+          'div',
+          null,
+          _react2.default.createElement('input', { type: 'text', placeholder: 'Enter a summoner name', id: 'name-input' }),
+          _react2.default.createElement(
+            _reactBootstrap.Button,
+            { type: 'submit', className: 'btn btn-success btn-xs', onClick: function onClick() {
+                return _this2.handleSubmit(document.getElementById("name-input").value);
+              } },
+            'Search'
+          )
+        ),
+        _react2.default.createElement(
+          'div',
+          null,
+          _react2.default.createElement(
+            'h2',
+            null,
+            'render player stats here'
+          ),
+          _react2.default.createElement(_MadLib2.default, { sending: this.state.test })
+        )
+      );
+    }
+  }]);
+
+  return PlayerStats;
+}(_react2.default.Component);
+
+exports.default = PlayerStats;
+
+/***/ }),
 /* 218 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -43852,109 +43954,6 @@ var valueEqual = function valueEqual(a, b) {
 };
 
 exports.default = valueEqual;
-
-/***/ }),
-/* 480 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__(0);
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactBootstrap = __webpack_require__(361);
-
-var _MadLib = __webpack_require__(216);
-
-var _MadLib2 = _interopRequireDefault(_MadLib);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-// var death_obj = {
-//     "type": event.type,
-//     "timestamp": event.timestamp,
-//     "position": {
-//         "x": event.position.x,
-//         "y": event.position.y
-//     },
-//     "killerId": event.killerId,
-//     "victimId": event.victimId
-// }
-
-var PlayerStats = function (_React$Component) {
-  _inherits(PlayerStats, _React$Component);
-
-  function PlayerStats(props) {
-    _classCallCheck(this, PlayerStats);
-
-    var _this = _possibleConstructorReturn(this, (PlayerStats.__proto__ || Object.getPrototypeOf(PlayerStats)).call(this, props));
-
-    _this.state = {
-      test: 'testing testing'
-    };
-    return _this;
-  }
-
-  _createClass(PlayerStats, [{
-    key: 'handleSubmit',
-    value: function handleSubmit(input) {
-      fetch('/getdeathinfo/' + input).then(function (data) {
-        console.log(data);
-      });
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
-
-      console.log(this.state.test);
-      return _react2.default.createElement(
-        'div',
-        null,
-        _react2.default.createElement(
-          'div',
-          null,
-          _react2.default.createElement('input', { type: 'text', placeholder: 'Enter a summoner name', id: 'name-input' }),
-          _react2.default.createElement(
-            _reactBootstrap.Button,
-            { type: 'submit', className: 'btn btn-success btn-xs', onClick: function onClick() {
-                return _this2.handleSubmit(document.getElementById("name-input").value);
-              } },
-            'Search'
-          )
-        ),
-        _react2.default.createElement(
-          'div',
-          null,
-          _react2.default.createElement(
-            'h2',
-            null,
-            'render player stats here'
-          ),
-          _react2.default.createElement(_MadLib2.default, { sending: this.state.test })
-        )
-      );
-    }
-  }]);
-
-  return PlayerStats;
-}(_react2.default.Component);
-
-exports.default = PlayerStats;
 
 /***/ })
 /******/ ]);
