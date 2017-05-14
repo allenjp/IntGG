557906c908bb21008c3ddecc015d92954e677c6a
Jeff <jeffreypallen3@gmail.com>
2017-05-14 13:34:37 -0400

    Adding additional route to get timeline data

--- server/services/api/routes/apiRoutes.js
+++ server/services/api/routes/apiRoutes.js
@@ -1,6 +1,9 @@
 module.exports = function(app) {
     var api = require('../controllers/apiController');
 
-    app.route('/getinfo/:summonername')
+    app.route('/api/getdeathinfo/:summonername')
         .get(api.getSummonerInfo)
+    
+    app.route('/api/getmatchtimeline/:matchid/:accountid')
+        .get(api.getmatchtimeline)
 };
