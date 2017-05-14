e0c89b37e2010d5f17e1b2ef46e12f337d7e2556
Jeff <jeffreypallen3@gmail.com>
2017-05-14 13:35:04 -0400

    Adding express rate limiter

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+var RateLimit = require('express-rate-limit');
+
+var apiLimiter = new RateLimit({
+    windowMs: 60 * 1000, // 1 minute window
+    delayAfter: 1,
+    delayMs: 1 * 1000,
+    max: 20
+});
 
 app.use(express.static('./client/public'));
+app.use('/api/', apiLimiter);
 
 var routes = require('../server/services/api/routes/apiRoutes');
 routes(app);
